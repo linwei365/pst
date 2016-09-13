53071aaccb96aef709b821836a8e807019f46bf8
Lin Wei <linwei@Lins-MacBook-Pro.local>
2016-09-12 18:41:19 -0700

    added channel

---
+++ NSURLSessionB/Channel.swift
@@ -0,0 +1,22 @@
+//
+//  Videos.swift
+//  NSURLSessionB
+//
+//  Created by Lin Wei on 9/12/16.
+//  Copyright © 2016 Lin Wei. All rights reserved.
+//
+
+import UIKit
+
+class Channel: NSObject {
+    
+    var profileImageUrl: String?
+    
+    var name: String?
+    var time:String?
+    
+    
+    
+    
+    
+}
